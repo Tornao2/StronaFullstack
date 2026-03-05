0a0ef9f2418480a2182a977165147af9e60ce803
Tornao2 <szymon.zuber2003@gmail.com>
2026-03-05 13:54:57 +0100

    Set up everything

---
+++ backend/src/main/java/StronaSklepowa/Uczelnia/UczelniaApplication.java
@@ -0,0 +1,13 @@
+package StronaSklepowa.Uczelnia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UczelniaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UczelniaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/StronaSklepowa/Uczelnia/UczelniaApplicationTests.java
@@ -0,0 +1,13 @@
+package StronaSklepowa.Uczelnia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UczelniaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
